52ef1798a7370d5287dbd993f4ff7218a1b8fc94
Hasan <imtiazhasan873@gmail.com>
2024-11-23 18:10:17 +0500

    isFull and isEmpty func

--- Array/Array_1D.java
+++ Array/Array_1D.java
@@ -1,12 +1,33 @@
 public class Array_1D {
-    public static void traversal(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
+    private int arr[];
+    private int size;
+
+    public Array_1D(int capacity){
+        arr = new int[capacity];
+        size = 0;
     }
+
+    public boolean isFull(){
+        return size == arr.length;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void traversal(){
+        if(isEmpty()){
+            System.out.println("Array is Empty");
+        } else{
+            for(int i=0;i<size;i++){
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[] = {1,3,5,7,8,9,11};
-        traversal(arr);
+        Array_1D obj = new Array_1D(10);
+        obj.traversal();
     }
 }

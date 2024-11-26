04f1bbfd41704ac4b806954a2cf3395341716299
hasan8733 <imtiazhasan873@gmail.com>
2024-11-26 06:10:08 +0500

    isFull and isEmpty func

--- Array/Array_2D.java
+++ Array/Array_2D.java
@@ -2,13 +2,30 @@ public class Array_2D {
     private int[][] arr;
     private int row;
     private int col;
+    private int rowSize;
+    private int colSize;
 
-    public Array_2D(int rowCapacity, int colCapacity){
+    public Array_2D(int rowCapacity, int colCapacity) {
         arr = new int[rowCapacity][colCapacity];
         row = 0;
         col = 0;
+        rowSize = rowCapacity;
+        colSize = colCapacity;
     }
+
+    public boolean isEmpty() {
+        return (row == 0 && col == 0);
+    }
+
+    public boolean isFull() {
+        return (row == rowSize && col == colSize);
+    }
+
+    public void traversal() {
+
+    }
+
     public static void main(String[] args) {
-        Array_2D obj = new Array_2D(5, 5);
+        Array_2D obj = new Array_2D(5, 3);
     }
 }

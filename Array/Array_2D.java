e575b892dbde1206dbd701787e3d5ed71e4ee4a3
hasan8733 <imtiazhasan873@gmail.com>
2024-12-10 05:10:22 +0500

    fix traversal issue

--- Array/Array_2D.java
+++ Array/Array_2D.java
@@ -26,8 +26,15 @@ public class Array_2D {
             System.out.println("Array is Empty");
         } else {
             for (int i = 0; i < row; i++) {
+                for (int j = 0; j < colSize; j++) {
+                    System.out.print(arr[i][j] + ", ");
+                }
+                System.out.println();
+            }
+
+            if (col > 0 && row < rowSize) {
                 for (int j = 0; j < col; j++) {
-                    System.out.print(arr[i][j] + " ");
+                    System.out.print(arr[row][j] + ", ");
                 }
                 System.out.println();
             }
@@ -39,11 +46,10 @@ public class Array_2D {
             System.out.println("Array is Full");
         } else {
             arr[row][col] = num;
-            if (col + 1 == colSize && row + 1 != rowSize) {
+            col++;
+            if (col == colSize) {
                 row++;
-                col = 0;
-            } else {
-                col++;
+                col = row == rowSize ? col : 0;
             }
         }
     }

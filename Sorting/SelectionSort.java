86d98d9561ed01d6a4a52029a128334f00ef81b1
hasan8733 <imtiazhasan873@gmail.com>
2025-01-07 02:25:52 +0500

    selection sort

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,35 @@
+package Sorting;
+
+public class SelectionSort {
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void sorting(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 8, 5, 3, 2, 7, 9, 1, 6 };
+        System.out.println("Before Sorting:");
+        print(arr);
+        sorting(arr);
+        System.out.println("After Sorting:");
+        print(arr);
+    }
+}

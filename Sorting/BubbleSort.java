e7e1788025e559e17ce1a82884cba51c60e038dd
hasan8733 <imtiazhasan873@gmail.com>
2025-01-07 01:57:26 +0500

    pull

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,19 @@
+package Sorting;
+
+public class BubbleSort {
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void sorting(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 8, 5, 3, 2, 7, 9, 1, 6 };
+        System.out.println("Before Sorting:");
+        print(arr);
+    }
+}
